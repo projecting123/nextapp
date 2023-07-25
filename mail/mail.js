4a61392aa086f1f9fcd98d6dd594dc4522ad7272
Ankur <dekalasit@gmail.com>
2023-07-26 00:11:26 +0530

    deploy commit

--- mail/mail.js
+++ mail/mail.js
@@ -6,23 +6,23 @@ import nodemailer from 'nodemailer'
 connect()
 
 
-export async function Mail({userId, email, emailType}){
+export async function Mail({ userId, email, emailType }) {
 
     const hashedToken = await bcryptjs.hash(userId.toString(), 10)
-    if(emailType == "VERIFY"){
-        await User.findByIdAndUpdate(userId, {verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 10000})
+    if (emailType == "VERIFY") {
+        await User.findByIdAndUpdate(userId, { verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 10000 })
     }
 
-    if(emailType == "FORGOT_PASSWORD_VERIFY"){
-        await User.findByIdAndUpdate(userId, {forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 10000})
+    if (emailType == "FORGOT_PASSWORD_VERIFY") {
+        await User.findByIdAndUpdate(userId, { forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 10000 })
     }
 
     const transporter = nodemailer.createTransport({
         host: "sandbox.smtp.mailtrap.io",
         port: 2525,
         auth: {
-          user: process.env.MAIL_USER,
-          pass: process.env.MAIL_PASS
+            user: "f851deea2b2a7f",
+            pass: "35c30565721ffb"
         }
     });
 
